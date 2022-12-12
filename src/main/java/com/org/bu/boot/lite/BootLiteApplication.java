f04eca6f2019cb5cac899a806c66b24bf37a3028
vidhirkaup <vidhir.kaup@gmail.com>
2022-12-11 20:02:36 -0500

    initial commit

---
+++ src/main/java/com/org/bu/boot/lite/BootLiteApplication.java
@@ -0,0 +1,13 @@
+package com.org.bu.boot.lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/bu/boot/lite/BootLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.bu.boot.lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
